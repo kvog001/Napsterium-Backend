3acfdb626543220de32fa2192a0a0925d6311348
Kamber Vogli <kambervogli@gmail.com>
2023-02-22 00:05:53 +0100

    try to setup connection with the client
    created SSL certificates and exported it

--- server.go
+++ server.go
@@ -2,18 +2,38 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+	"io/ioutil"
+	"crypto/tls"
 )
 
 func main() {
-	http.HandleFunc("/helloworld", helloHandler)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/helloworld", helloHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
+	// Create the TLS config
+    	config := &tls.Config{
+        	MinVersion: tls.VersionTLS10,
+        	MaxVersion: tls.VersionTLS13,
+    	}
 
-	if err := http.ListenAndServe("193.233.202.119:8080", nil); err != nil {
-		log.Fatal(err)
-	}
+    	// Create the HTTP server with the TLS config
+    	server := &http.Server{
+       		Addr:      "193.233.202.119:8080",
+		Handler:  mux,
+        	TLSConfig: config,
+    	}
+
+    	// Listen and serve with TLS
+    	err := server.ListenAndServeTLS("server.crt", "server.key")
+    	if err != nil {
+		fmt.Println("Error starting server:", err)
+    	}
+
+	//if err := http.ListenAndServe("193.233.202.119:8080", nil); err != nil {
+	//	log.Fatal(err)
+	//}
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
@@ -22,11 +42,24 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    if r.Method != "GET" {
+    if r.Method != "POST" {
         http.Error(w, "Method is not supported.", http.StatusNotFound)
         return
     }
 
+    // Read the request body into a string
+    body, err := ioutil.ReadAll(r.Body)
+    if err != nil {
+        http.Error(w, "Error reading request body.", http.StatusInternalServerError)
+        return
+    }
+    defer r.Body.Close()
+    requestText := string(body)
 
+    // Print the request text to the server console
+    fmt.Printf("Received request: %s\n", requestText)
+
+    // Return a response
     fmt.Fprintf(w, "Hello World!\n")
 }
+
